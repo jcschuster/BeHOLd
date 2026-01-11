362f8c12f626da903d655c779f78a98501a4ce96
johannes-christian.schuster <johannes-christian.schuster@stud.uni-bamberg.de>
2026-01-11 17:38:52 +0100

    fix math parsing in documentation

--- mix.exs
+++ mix.exs
@@ -84,9 +84,7 @@ defmodule BeHOLd.MixProject do
             renderMathInElement(document.body, {
               delimiters: [
                 {left: "$$", right: "$$", display: true},
-                {left: "$", right: "$", display: false},
-                {left: "\\(", right: "\\)", display: false},
-                {left: "\\[", right: "\\]", display: true}
+                {left: "$", right: "$", display: false}
               ]
             });
           }
